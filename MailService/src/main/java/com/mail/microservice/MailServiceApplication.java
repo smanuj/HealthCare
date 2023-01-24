176ca542f71aa9f53eed0329a88f70adb9f29b39
Anuj S M <Anuj.SM@valtech.com>
2023-01-24 17:03:51 +0530

    Linking with login and hospital Microservices

--- MailService/src/main/java/com/mail/microservice/MailServiceApplication.java
+++ MailService/src/main/java/com/mail/microservice/MailServiceApplication.java
@@ -2,12 +2,21 @@ package com.mail.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MailServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MailServiceApplication.class, args);
+	}
+	
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 
 }
