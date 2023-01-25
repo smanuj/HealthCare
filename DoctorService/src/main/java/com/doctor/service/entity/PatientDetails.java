5d38e6033fe209f431f87b545cfc7317aea5f6d1
Anuj S M <Anuj.SM@valtech.com>
2023-01-25 11:36:21 +0530

    Switching to oracle

--- DoctorService/src/main/java/com/doctor/service/entity/PatientDetails.java
+++ DoctorService/src/main/java/com/doctor/service/entity/PatientDetails.java
@@ -11,7 +11,7 @@ import javax.persistence.OneToOne;
 public class PatientDetails {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
 	private int id;
 	@OneToOne(targetEntity = AadharDetails.class)
 	@JoinColumn(name = "aId", referencedColumnName = "aId")
